314a98a1cd816be04a5135ddd0f3bebd43ca2501
kyle.stanley <therealkylestanley@gmail.com>
2016-08-10 13:43:50 +0000

    huh?

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   resources :microposts
   resources :users
-=======
->>>>>>> 49c3ab0bd7ba0d532047cb58eccbcadc404d784f
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
